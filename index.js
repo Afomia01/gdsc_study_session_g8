35d119a98100bcc70cdd3b6dcc48c34d85b74047
Afomia Dugassa <afomialink@gmail.com>
2023-11-30 19:27:50 +0300

    week 2 commit

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("Hello world");
+//let, const block scope
+//var function scope *hoisting-will know them  before declaration
+//redeclaration 
+/* non primitve - object , array*/
+//use === for equality
+const mass= prompt("Enter your weight");
+const height= prompt("Enter your height");
+
+const BMI= mass/(Math.pow(height,2));
+console.log("BMI:" +BMI);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Task 2
+let num1= document.getElementById("first").value;
+let num2= document.getElementById("second").value;
+let age1=1;
+let age=19;
+let sum= age+ age1;
+let diff= age- age1;
+let product= age *age1;
+let quotient= age/age1;
+
+console.log(sum);
+console.log(diff);
+console.log()
+console.log(quotient);
+*/
+
