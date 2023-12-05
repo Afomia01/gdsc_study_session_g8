3af3d01808ebaf0b6704eaeaeed3aa95a661e36a
Afomia Dugassa <afomialink@gmail.com>
2023-12-05 08:25:41 +0300

    Week 3 commit

--- index.js
+++ index.js
@@ -1,47 +1,37 @@
-//let, const block scope
-//var function scope *hoisting-will know them  before declaration
-//redeclaration 
-/* non primitve - object , array*/
-//use === for equality
-const mass= prompt("Enter your weight");
-const height= prompt("Enter your height");
+const input= document.getElementById("input");
+const task= document.getElementById("tasks");
+document.getElementById("MyBtn").onclick= function(){
+    if(input.value===''){
+        alert("Enter TASKS! I'm sure you got a lot to do")
+    }
+    else{
+        let li= document.createElement("li");
+        li.innerHTML= input.value;
+        task.appendChild(li);
+        let span= document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
 
-const BMI= mass/(Math.pow(height,2));
-console.log("BMI:" +BMI);
+    }
+    input.value = "";
+    saveData();
 
+}
+task.addEventListener("click", function(e){
+    if(e.target.tagName=== "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+function saveData(){
+    localStorage.setItem("data", task.innerHTML);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*Task 2
-let num1= document.getElementById("first").value;
-let num2= document.getElementById("second").value;
-let age1=1;
-let age=19;
-let sum= age+ age1;
-let diff= age- age1;
-let product= age *age1;
-let quotient= age/age1;
-
-console.log(sum);
-console.log(diff);
-console.log()
-console.log(quotient);
-*/
-
+}
+function showTask(){
+    task.innerHTML = localStorage.getItem("data")
+}
+showTask();
