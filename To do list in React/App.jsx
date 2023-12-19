1f2eb4ed49360703ce2e747916d77f3ec87d0897
Afomia01 <150925057+Afomia01@users.noreply.github.com>
2023-12-19 23:00:45 +0300

    Month 2 week 1

---
+++ To do list in React/App.jsx
@@ -0,0 +1,22 @@
+import Card from './Card.jsx'
+import Student from './Student.jsx'
+import './App.css'
+import Greetings from './Greetings.jsx'
+import Todo from './Todo.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+
+return(
+  <>
+  <Todo/>
+
+
+
+
+  </>
+);
+}
+
+export default App
